6806a97b59ff506307ab3782faae29b8b303730b
mibbim <michele.berti.g@gmail.com>
2022-02-09 10:29:04 +0100

    fixed missing module load

--- hybrid/strong/4_strong.sh
+++ hybrid/strong/4_strong.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
  
 #PBS -q dssc
-#PBS -l nodes=4:ppn=24
+#PBS -l nodes=1:ppn=24
 #PBS -l walltime=1:00:00
+
+module load openmpi
 cd $PBS_O_WORKDIR
 
-for N in 4, 8; do
+for N in 4 8; do
 for n in 1 2 4 8 12; do
-    mpirun --map-by socket -np ${N} ../nov_hybrid_kd_tree.x 100000000 ${n} > tree_4_${n}.dot 2>>time_4.out
+    mpirun --map-by socket -np ${N} ../nov_hybrid_kd_tree.x 100000000 ${n} 2>>time_${N}.out
 done
 done
