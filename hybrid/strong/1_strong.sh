255ea26d77996b5c5597eb8001cfb9e792b50b46
mibbim <michele.berti.g@gmail.com>
2022-02-09 09:53:30 +0100

    removed tree put file

--- hybrid/strong/1_strong.sh
+++ hybrid/strong/1_strong.sh
@@ -9,6 +9,6 @@ cd $PBS_O_WORKDIR
 
 for N in 1 2; do
 for n in 1 2 4 8 12; do
-    mpirun --map-by socket -np ${N} ../nov_hybrid_kd_tree.x 100000000 ${n} > tree_${N}_${n}.dot 2>>time_${N}.out
+    mpirun --map-by socket -np ${N} ../nov_hybrid_kd_tree.x 100000000 ${n} 2>>time_${N}.out
 done
 done
