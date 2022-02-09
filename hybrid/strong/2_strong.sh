4e5939931e7ece06fe40f23ec2fcfcf1c0cabafb
mibbim <michele.berti.g@gmail.com>
2022-02-09 10:28:21 +0100

    Initial commit

---
+++ hybrid/strong/2_strong.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ 
+#PBS -q dssc
+#PBS -l nodes=1:ppn=24
+#PBS -l walltime=1:00:00
+
+module load openmpi
+cd $PBS_O_WORKDIR
+
+for N in 2 4; do
+for n in 1 2 4 8 12; do
+    mpirun --map-by socket -np ${N} ../nov_hybrid_kd_tree.x 100000000 ${n} 2>>time_${N}.out
+done
+done
